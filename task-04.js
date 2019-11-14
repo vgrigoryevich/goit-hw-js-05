6d78a3902381e30991bfe3b616413222056d09a7
vgrigoryevich <v.grigoryevich@gmail.com>
2019-11-14 22:58:52 +0200

    its all

--- task-04.js
+++ task-04.js
@@ -1,3 +1,26 @@
+class StringBuilder {
+    constructor(value) {
+        this._value = value;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    append(value) {
+        this._value = this._value + value;
+    }
+
+    prepend(value) {
+        this._value = value + this._value;
+    }
+
+    pad(value) {
+        this._value = value + this._value + value;
+    }
+}
+
+
 const builder = new StringBuilder('.');
 
 builder.append('^');
